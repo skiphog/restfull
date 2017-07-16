e9339d67e991de9b18fea8e21fd637150b839a36
Skip <skiphog@mail.ru>
2017-07-16 15:19:19 +0300

    Pagination of Results

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -2,8 +2,10 @@
 
 namespace App\Traits;
 
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Validator;
 
 trait ApiResponser
 {
@@ -27,11 +29,34 @@ trait ApiResponser
 
         $collection = $this->filterData($collection, $transformer);
         $collection = $this->sortData($collection, $transformer);
+        $collection = $this->paginate($collection);
 
         return $this->successResponse(
             $this->transformData($collection, $transformer),
             $code
         );
+    }
+
+    protected function paginate(Collection $collection)
+    {
+        Validator::validate(request()->all(), [
+            'per_page' => 'integer|min:2|max:50'
+        ]);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 15;
+        if (request()->has('per_page')) {
+            $perPage = (int)request()->input('per_page');
+        }
+
+        $result = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($result, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath()
+        ]);
+        $paginated->appends(request()->all());
+
+        return $paginated;
     }
 
     protected function showOne(Model $model, $code = 200)
